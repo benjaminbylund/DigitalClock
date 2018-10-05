09f09497e877463e8fa1fd7a6dc84915274b2677
benjaminbylund <te16.benjamin.bylund@umea.nti.se>
2018-10-05 13:07:52 +0200

    Har skrivit av flödesdiagramet.

---
+++ src/Clock.java
@@ -0,0 +1,31 @@
+public class Clock {
+
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+    private String displayString;
+
+    public Clock(){
+
+    }
+
+    public Clock(int minutes, int hours){
+
+    }
+
+    public void timeTick(){
+
+    }
+                    //int, int
+    public void setTime(){
+
+    }
+
+    public String getTime(){
+        return hours.getDisplayValue();
+    }
+
+    private void updateDisplay(){
+
+    }
+
+}
---
+++ src/NumberDisplay.java
@@ -0,0 +1,25 @@
+public class NumberDisplay {
+    private int limit;
+    private int value;
+
+    public NumberDisplay(){
+        this.limit = limit;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getDisplayValue(){
+        return null;
+    }
+
+    public void increment(){
+
+    }
+}
